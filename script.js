05c345686a1d9f2d0e5724d0dec9df1ba9e09212
Grace Akintoye <156701625+GraceAkintos@users.noreply.github.com>
2024-07-12 22:23:39 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var welcomeModal = new bootstrap.Modal(document.getElementById('welcomeModal'));
+  welcomeModal.show();
+});
+console.log(" Gracie and G developed this. Any suggestions can be sent to our linkedIn or Git");
+console.log(" G; LinkedIN: Guillaume Girishya");
+console.log(" Gracie; LinkedIN: Grace Akintoye");
