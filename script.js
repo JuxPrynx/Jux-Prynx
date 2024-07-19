8caf588d8de8cbc3762819727119196f41fc2deb
Grace Akintoye <156701625+GraceAkintos@users.noreply.github.com>
2024-07-19 05:49:58 +0000

    modal video autoplay update

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
   welcomeModal.show();
 });
 
+
 console.log(" Gracie and G developed this. Any suggestions can be sent to our linkedIn or Git");
 console.log(" G; LinkedIN: Guillaume Girishya");
 console.log(" Gracie; LinkedIN: Grace Akintoye");
